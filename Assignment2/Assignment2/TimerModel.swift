acce8f8a4e173b1a69cd4902372f29ef39f719dd
Rory O'Connor <roryoconnor@outlook.co.nz>
2017-10-03 13:55:39 +1300

    Added timer and system time display.

---
+++ Assignment2/Assignment2/TimerModel.swift
@@ -0,0 +1,64 @@
+//
+//  TimerModel.swift
+//  Timer
+//
+//  Designed by Lech Szymanski in 2014.
+//  Swift implementation by David Eyers in 2015.
+//  Updated for Swift 3.x / Xcode 8.3 in 2017 by David Eyers.
+//
+
+import Foundation
+
+// classes implement this protocol to receive callbacks from the TimerModel
+public protocol TimerModelDelegate {
+    func secondsChanged(_ second:Int)
+}
+
+public class TimerModel: NSObject {
+    var seconds:Int = 0
+    var stopping:Bool = false
+    
+    public fileprivate(set) var running:Bool = false
+    public var delegate:TimerModelDelegate? = nil
+    
+    func alertDelegate() {
+        // if a delegate isn't defined, do nothing
+        delegate?.secondsChanged(seconds)
+    }
+    
+    // the OS's NSTimer will callback here
+    func countUp(_ theTimer:Foundation.Timer) {
+        if(!stopping) {
+            seconds += 1
+            alertDelegate()
+        } else {
+            theTimer.invalidate()
+            running = false
+            stopping = false
+        }
+    }
+    
+    public func start() {
+        if(running) { return }
+        
+        // Create a timer object that calls the countUp method every second
+        let timer = Foundation.Timer(timeInterval: 1, target: self, selector: #selector(TimerModel.countUp(_:)), userInfo: nil, repeats: true)
+        
+        // Get a reference to the current event loop
+        let loop = RunLoop.current
+        
+        // Attach the above timer to the event loop:
+        // it will then actually start firning
+        loop.add(timer, forMode: RunLoopMode.commonModes)
+        
+        // Change our internal state to running
+        running = true
+    }
+    public func stop() {
+        stopping = true
+    }
+    public func reset() {
+        seconds = 0
+        alertDelegate()
+    }
+}
---
+++ Assignment2/Assignment2/TimerViewController.swift
@@ -0,0 +1,119 @@
+//
+//  TimerViewController.swift
+//  Assignment2
+//
+//  Created by Rory O'Connor on 3/10/17.
+//  Copyright © 2017 Rory O'Connor. All rights reserved.
+//
+
+import Cocoa
+
+class TimerViewController: NSViewController, TimerModelDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    // Outlet to the label that displays the formatted timer value
+    @IBOutlet weak var timerDisplay: NSTextField!
+    
+    // Outlet to the label that displays the formatted system time value
+    @IBOutlet weak var systemTimeDisplay: NSTextField!
+    
+    // Outlets to the buttons at the bottom of the window
+    @IBOutlet weak var startStopButton: NSButton!
+    @IBOutlet weak var resetButton: NSButton!
+    
+    // Outlets to the menus that we added
+    //@IBOutlet weak var startMenuItem: NSMenuItem!
+    //@IBOutlet weak var stopMenuItem: NSMenuItem!
+    
+    // Timer state is now in an instance of the model
+    var timer:TimerModel? = nil
+    
+    //date formatter to format system time
+    var dateFormatter = DateFormatter()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // create an instance of a TimerModel and reset it
+        timer = TimerModel()
+        timer!.delegate = self
+        timer!.reset()
+        
+        // Create a timer object that calls the updateSystemTime method every second
+        let systemTimer = Foundation.Timer(timeInterval: 1, target: self, selector: #selector(self.updateSystemTime(_:)), userInfo: nil, repeats: true)
+        
+        //get a reference to the current run loop
+        let loop = RunLoop.current
+        
+        //attach timer to the event loop
+        loop.add(systemTimer, forMode: RunLoopMode.commonModes)
+        
+        //format the time
+        dateFormatter.timeStyle = .medium
+    }
+    
+    //update the system time display
+    func updateSystemTime(_ theTimer:Foundation.Timer){
+        systemTimeDisplay.stringValue = "\(dateFormatter.string(from: Date()))"
+    }
+    
+    // Update the label to show the current time, formatted for display
+    // callback target from TimerModel
+    func secondsChanged(_ seconds: Int) {
+        // Take the total number of seconds the timer has run for
+        var s = seconds
+        // Determine how many hours and minutes this represents
+        let h = s/3600
+        s %= 3600
+        let m = s/60
+        s %= 60
+        
+        // Update the label with the formatted timer value
+        // (Rather ObjCly!)
+        timerDisplay.stringValue = String(format:"%02ld:%02ld:%02ld", h,m,s)
+    }
+    
+    // Start the timer
+    func startTimer() {
+        timer!.start()
+        
+        // Update the Start/Stop button label and state of the reset button
+        startStopButton.title = "Stop"
+        resetButton.isEnabled = false
+    }
+    
+    // Stop the timer
+    func stopTimer() {
+        timer!.stop()
+        
+        // Update the Start/Stop button label and state of the reset button
+        startStopButton.title = "Start"
+        resetButton.isEnabled = true
+    }
+    
+    // Toggle the state of the timer
+    @IBAction func startStopAction(_ sender:AnyObject) {
+        if(timer!.running) {
+            stopTimer()
+        } else {
+            startTimer()
+        }
+    }
+    
+    // The reset action zeros the counter
+    @IBAction func resetAction(_ sender:AnyObject){
+        timer!.reset()
+    }
+    
+    // When users interact with a menu, the system checks whether items should be greyed out.
+    // We use our "running" state to determine whether to grey out start or stop.
+//    override func validateMenuItem(_ menuItem: NSMenuItem) -> Bool {
+//        if(menuItem == startMenuItem) {
+//            return !timer!.running
+//        } else if (menuItem == stopMenuItem) {
+//            return timer!.running
+//        }
+//        return true
+//    }
+}
